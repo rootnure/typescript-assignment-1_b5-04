c777bd5ce859d25d23091f459d1334328f1e1e26
rootnure <nur.diu.2791@gmail.com>
2025-05-08 22:36:57 +0600

    Problem 8 Solution added

--- src/assignment-solution.ts
+++ src/assignment-solution.ts
@@ -86,3 +86,18 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
 
 // console.log(getMostExpensiveProduct(products));
 // // Output: { name: "Bag", price: 50 }
+
+async function squareAsync(n: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+        if (n >= 0) {
+            setTimeout(() => {
+                resolve(n * n);
+            }, 1000)
+        } else {
+            reject(new Error("Negative number not allowed"));
+        }
+    })
+}
+
+squareAsync(4).then(console.log);        // Output after 1s: 16
+squareAsync(-3).catch(console.error);    // Output: Error: Negative number not allowed
