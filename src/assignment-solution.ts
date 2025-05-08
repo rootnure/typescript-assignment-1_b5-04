b1f9ad4763ee491d05e5c80c232d09a553b8e6e2
rootnure <nur.diu.2791@gmail.com>
2025-05-08 22:08:45 +0600

    Problem 4 Solution added

--- src/assignment-solution.ts
+++ src/assignment-solution.ts
@@ -31,6 +31,26 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
     return newArr;
 }
 
-console.log(concatenateArrays<string>(["a", "b"], ["c"]));      // Output: ["a", "b", "c"]
-console.log(concatenateArrays<number>([1, 2], [3, 4], [5]));     // Output: [1, 2, 3, 4, 5]
+// console.log(concatenateArrays<string>(["a", "b"], ["c"]));      // Output: ["a", "b", "c"]
+// console.log(concatenateArrays<number>([1, 2], [3, 4], [5]));     // Output: [1, 2, 3, 4, 5]
+
+class Vehicle {
+    constructor(private make: string, private year: number) { }
+    getInfo() {
+        console.log(`Make: ${this.make}, Year: ${this.year}`);
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make: string, year: number, private model: string) {
+        super(make, year);
+    }
+    getModel() {
+        console.log(`Model: ${this.model}`);
+    }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
+myCar.getModel();  // Output: "Model: Corolla"
 
